8a3f7f6b93fc97793a93cdb941daf36c1f95a864
Ikram khan <ikcode4599@gmail.com>
2024-06-08 00:38:26 +0500

    first commit

---
+++ src/anime.ts
@@ -0,0 +1,20 @@
+// // Animation Button 
+// const animateButton = (e: MouseEvent): void => {
+//     e.preventDefault();
+//     // reset animation
+//     (e.target as HTMLElement).classList.remove('animate');
+    
+//     (e.target as HTMLElement).classList.add('animate');
+//     setTimeout(() => {
+//       (e.target as HTMLElement).classList.remove('animate');
+//     }, 700);
+//   };
+  
+//   const bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+//   for (let i = 0; i < bubblyButtons.length; i++) {
+//     bubblyButtons[i].addEventListener('click', animateButton as EventListener);
+//   }
+  
+const sample = require('./main.json');
+console.log(sample);
